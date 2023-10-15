23566a87a30efb706eb9580634fd0e301abee06c
FaresNassar <aborafat_999@hotmail.com>
2023-10-15 18:37:19 +0300

    first update add review

--- src/controllers/controller_review.ts
+++ src/controllers/controller_review.ts
@@ -1,13 +1,27 @@
 import express from 'express';
+import { Request, Response } from 'express';
+
 import dataSource from '../db/dataSource.js';
 import { ReviewNS } from '../../@types/type_review.js';
 import { Review } from '../db/entities/review.js'
+import { getRepository } from 'typeorm';
 
-
-const insertReview = async (payload: ReviewNS.Review) => {
-
-}
-
+// const insertReview = async (payload: ReviewNS.Review) => {
+//     try {
+//         const { userId, fullName, productId, comments } = req.body;
+  
+//         const reviewRepository = getRepository(Review);
+//         const newReview = reviewRepository.create({
+//           userId,
+//           fullName,
+//           productId,
+//           comments,
+//         });
+  
+//         await reviewRepository.save(newReview);
+  
+// }
+// }
 const updateReview = async (payload: ReviewNS.Review) => {
 
 }
@@ -22,7 +36,7 @@ const getReviews = () => {
 }
 
 export {
-    insertReview,
+    // insertReview,
     updateReview,
     deleteReview,
     getReviews
--- src/routes/route_review.ts
+++ src/routes/route_review.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import { insertReview } from '../controllers/controller_review';
 
 const route = express.Router();
 
-route.post('/create_review', (req, res) => {
-    console.log('create review route')
-    res.status(200).send('review created successfully');
-})
+
+
+route.post('/create_review', (req, res, next) => {
+    insertReview(req.body).then(data => {
+      res.status(201).send(data)
+    }).catch(error => {
+      res.status(500).send("something went wrong")
+    })
+  });
+// route.post('/create_review', (req, res) => {
+//     console.log('create review route')
+//     res.status(200).send('review created successfully');
+// })
 
 
 route.put('/update_review', (req, res) => {
