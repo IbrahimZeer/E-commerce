3e60bf81ccd73355276605dd7a90fdbcc2d57d15
FaresNassar <aborafat_999@hotmail.com>
2023-10-19 13:28:11 +0300

    updated

--- src/routes/route_order.ts
+++ src/routes/route_order.ts
@@ -1,25 +1,36 @@
 import express from 'express';
 import { deleteOrder, getOrders, insertOrder, updateOrder } from '../controllers/controller_order.js';
 import { OrderNS } from '../../@types/type_order.js';
-import { getRepository } from 'typeorm';
 import { Order } from '../db/entities/orders/Order.js';
 import { authenticate } from '../middleware/authentication.js';
+import { getManager } from 'typeorm';
+
 const route = express.Router();
 
 route.post('/create_order', async (req, res) => {
-    const payload: OrderNS.Order = req.body;
-    const newOrder = await insertOrder(payload);
-    res.json(newOrder);
+    try {
+        const payload: OrderNS.Order = req.body;
+        const newOrder = await insertOrder(payload);
+        res.status(201).json(newOrder);
+    } catch {
+        res.status(500).json({ error: 'Failed to create the order' });
 
+
+    }
 })
 
 
-route.put('/update_order/:orderId', async (req, res) => {
-    const orderId: string = req.params.orderId;
-    const payload: OrderNS.Order = req.body;
-    const updatedOrder = await updateOrder(orderId, payload);
-    res.json(updatedOrder);
-})
+route.put('/update_order:orderId', async (req, res) => {
+    try {
+        const orderId: string = req.params.orderId;
+        const payload: OrderNS.Order = req.body;
+        const updatedOrder = await updateOrder(orderId, payload);
+        res.status(200).json(updatedOrder);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update the order' });
+    }
+});
+
 
 
 route.delete('/delete_order/:orderId', async (req, res) => {
