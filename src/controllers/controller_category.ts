19a7e26959657274a63c98baddc0afb7fe66a8cf
ibraheem Zeer <211400@ppu.edu.ps>
2023-10-20 13:32:54 +0300

    get all and get by id of category

--- src/controllers/controller_category.ts
+++ src/controllers/controller_category.ts
@@ -4,7 +4,7 @@ import { Category } from '../db/entities/Products/Category.js';
 
 
 
-const insertCategory = async (payload: CategoryNs.Category) => {
+const insertCategoryController = async (payload: CategoryNs.Category) => {
     try {
         const newCategory = new Category();
         newCategory.catName = payload.catName;
@@ -17,21 +17,16 @@ const insertCategory = async (payload: CategoryNs.Category) => {
     }
 }
 
-const updateCategory = async (id: number, payload: CategoryNs.Category) => {
+const updateCategoryController = async (id: number, payload: CategoryNs.Category) => {
     try {
         const category = await Category.findOne({ where: { id } });
-
         if (!category) {
             throw new Error('Product not found');
         }
-
         // Update the product properties
         category.catName = payload.catName;
         category.catDes = payload.catDes;
-
-
         await category.save();
-
         return category;
     } catch (error) {
         throw new Error(`Failed to update the product`);
@@ -39,7 +34,7 @@ const updateCategory = async (id: number, payload: CategoryNs.Category) => {
 };
 
 
-const delete_categorie = async (id: number, payload: CategoryNs.Category) => {
+const deleteCategoryController = async (id: number, payload: CategoryNs.Category) => {
     try {
         // Find the product based on the provided ID and delete it
         const category = await Category.findOneBy({ id });
@@ -52,9 +47,32 @@ const delete_categorie = async (id: number, payload: CategoryNs.Category) => {
         throw new Error(`Failed to delete the product`);
     }
 }
-export {
-    insertCategory,
-    updateCategory,
-    delete_categorie
 
+const getCategoriesController = async () => {
+    try {
+        const categories = await Category.find();
+        return categories;
+    } catch (error) {
+        throw new Error('Failed to fetch categories');
+    }
+}
+
+const getCategoryByIdController = async (id: number) => {
+    try {
+        const category = await Category.findOne({ where: { id } });
+        if (!category) {
+            throw new Error('Category not found');
+        }
+        return category;
+    } catch (error) {
+        throw new Error(`Failed to fetch the category`);
+    }
+}
+
+
+export {
+    insertCategoryController,
+    updateCategoryController,
+    deleteCategoryController,
+    getCategoriesController
 }
--- src/routes/route_categories.ts
+++ src/routes/route_categories.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-import { delete_categorie, insertCategory, updateCategory } from '../controllers/controller_category.js';
+import { deleteCategoryController, getCategoriesController, updateCategoryController, insertCategoryController } from '../controllers/controller_category.js';
 import { Category } from '../db/entities/Products/Category.js';
-import e from 'express';
 const route = express.Router();
 
 
 
 route.post('/add_categorie', async (req, res) => {
     try {
-        const newCategory = await insertCategory(req.body);
+        const newCategory = await insertCategoryController(req.body);
         res.status(201).json(newCategory);
     } catch (error) {
         res.status(500).json({ error: 'Failed to create the category' });
@@ -19,7 +18,7 @@ route.put('/update_categorie/:id', async (req, res) => {
     try {
         const id = parseInt(req.params.id, 10);
         // Assuming the request body contains the updated product data
-        const update = await updateCategory(id, req.body);
+        const update = await updateCategoryController(id, req.body);
 
         res.status(200).json(update);
     } catch (error) {
@@ -33,7 +32,7 @@ route.delete('/delete_categorie/:id', (req, res) => {
     const id = parseInt(req.params.id);
     Category.findOneBy({ id: id })
     if (id) {
-        delete_categorie(id, req.body).then(() => {
+        deleteCategoryController(id, req.body).then(() => {
             res.status(200).send('Category deleted successfully');
         }).catch(error => {
             res.status(401).send('category not found')
@@ -45,9 +44,27 @@ route.delete('/delete_categorie/:id', (req, res) => {
 
 
 route.get('/all_categorie', (req, res) => {
-    console.log('list of categories')
-    res.status(200).send('list of categories returned successfully');
+    getCategoriesController().then((categories) => {
+        res.status(200).json(categories);
+    }).catch(error => {
+        res.status(500).json({ error: 'Failed to fetch categories' });
+    })
 })
+
+route.get('/categorie/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    Category.findOneBy({ id: id })
+    if (id) {
+        getCategoriesController().then((categories) => {
+            res.status(200).json(categories);
+        }).catch(error => {
+            res.status(500).json({ error: 'Failed to fetch categories' });
+        })
+    } else {
+        res.status(404).send('something went wrong');
+    }
+})
+
 
 // get categorie by id , other thinks
 export default route;
