69ec71eeba5bf1fe1b10da7b9d2eec4d51451783
ibraheem Zeer <211400@ppu.edu.ps>
2023-10-29 22:54:24 +0200

    updated

--- app.ts
+++ app.ts
@@ -20,10 +20,15 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+app.use(express.static('public'));
+
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }))
+
 // routes of application
 app.use('/admin', adminRouter);
 app.use('/users', customerRouter);
