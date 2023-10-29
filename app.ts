fd883ae248d3e3cd7f59d4e11b7357f1eedb7573
ibraheem Zeer <211400@ppu.edu.ps>
2023-10-29 22:46:26 +0200

    updated

--- app.ts
+++ app.ts
@@ -20,9 +20,6 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
 
-
-app.set('view engine', 'ejs');
-
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
